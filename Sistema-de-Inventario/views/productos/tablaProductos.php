5738c247a42b4e36d5e01f1c16adf4cfe527b951
MoisesG59 <moises.sigaran22@itca.edu.sv>
2024-06-10 10:39:47 -0600

    actualizacion2

--- Sistema-de-Inventario/views/productos/tablaProductos.php
+++ Sistema-de-Inventario/views/productos/tablaProductos.php
@@ -86,19 +86,123 @@ $productos = $objProducto->mostrar();
 
 
 <!DOCTYPE html>
-<html lang="en">
+< lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="../../resources/src/Bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="../../resources/src/Bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../../resources/src/Bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../resources/src/Bootstrap/css/lobibox.css">
+    <link rel="stylesheet" href="../../resources/src/Bootstrap/css/select2.css">
+    <link rel="stylesheet" href="../../resources/src/Bootstrap/css/datatables.css">
+    <link rel="stylesheet" href="../../resources/src/Bootstrap/css/waitMe.css">
     <script src="../../resources/src/SweetAlert/sweetalert2.min.js"></script>
     <link rel="stylesheet" href="../../resources/src/SweetAlert/sweetalert2.min.css">
+    <!--Dependencias de terceros-->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/buttons/3.0.2/css/buttons.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
     <title>Productos</title>
+    <style>
+        .action-buttons {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .btn-spacing {
+            margin: 0 5px; /* Ajusta el margen según tus necesidades */
+        }
+        /* Estilo para centrar el enlace de Inicio */
+        .navbar-center {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+    </style>
 </head>
 
 <body class="bg-light">
+
+        <!-- Navbar -->
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand navbar-center" href="../../index.php">Inicio</a>
+            <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="menuInicio">Menú</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <?php if (!isset($_SESSION['Nombre'])): ?>
+                        <li class="nav-item">
+                            <a href="#" id="loginBtn" class="nav-link">Iniciar Sesión</a>
+                        </li>
+                        <?php else: ?>
+                        <li class="nav-item">
+                            <a href="#" id="loginBtn" class="nav-link">Panel de Control</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Administración
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" id="Usuarios" href="../../views/usuarios/tablaUsuario.php">Usuarios</a></li>
+                                <li><a class="dropdown-item" id="Categorias" href="#">Categorias</a></li>
+                                <li><a class="dropdown-item" id="Sucursales" href="#">Sucursales</a></li>
+                                <li><a class="dropdown-item" id="Proveedores" href="#">Proveedores</a></li>
+                                <li><a class="dropdown-item" id="Clientes" href="#">Clientes</a></li>
+                                <li>
+                                <hr class="dropdown-divider">
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../../views/compras/tablaCompras.php" id="Compras" class="nav-link">Compras</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" id="Productos" class="nav-link">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" id="Ventas" class="nav-link">Ventas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" id="Entradas" class="nav-link">Entradas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" id="Salidas" class="nav-link">Salidas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../../views/modifEmpresa/viewModifEmpresa.php" id="configurarEmpresa" class="nav-link">Configurar Empresa</a>
+                        </li>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+            </div>
+            <?php if (isset($_SESSION['Nombre'])): ?>
+            <div class="dropdown ms-auto">
+                <a class="navbar-brand dropdown-toggle" href="#" id="usuarioDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <?php echo $_SESSION['Nombre']; ?>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="usuarioDropdown">
+                    <li><a class="dropdown-item" href="#">Rol: <?php echo $_SESSION['Rol']; ?></a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="../../logout.php">Cerrar Sesión</a></li>
+                </ul>
+            </div>
+            <?php endif; ?>
+        </div>
+    </nav>
+    <br><br><br>
 
     <h1 class="text-center p-4">Tabla Productos</h1>
 
@@ -224,14 +328,76 @@ $productos = $objProducto->mostrar();
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.3/js/bootstrap.min.js"></script>
+    <!-- Bootstrap JS and dependencies -->
+<script src="../../resources/src/Bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../../resources/src/Bootstrap/js/bootstrap.min.js"></script>
+<script src="../../resources/src/Bootstrap/js/datatables.js"></script>
+<script src="../../resources/src/Bootstrap/js/datatables.min.js"></script>
+<script src="../../resources/src/Bootstrap/js/waitMe.min.js"></script>
+<script src="../../resources/src/Bootstrap/js/jquery.min.js"></script>
+<script src="../../resources/src/Bootstrap/js/popper.min.js"></script>
+<script src="../../resources/src/Bootstrap/js/lobibox.js"></script>
+<script src="../../resources/src/Bootstrap/js/notifications.js"></script>
+<script src="../../resources/src/Bootstrap/js/messageboxes.js"></script>
+<script src="../../resources/src/Bootstrap/js/select2.js"></script>
 </body>
-
 </body>
+<script>
+$(document).ready(function() {
+    // Inicializa DataTable
+    $("#tabla-datos").DataTable();
+    // Maneja el clic en el botón "Agregar registro"
+    $('.btn-primary').click(function() {
+        // Aquí puedes agregar código adicional si necesitas hacer algo antes de abrir el modal
+        $('#modal-agregar').modal('show');
+    });
+});
 
+// Maneja el clic en el botón "Guardar"
+$('#modal-agregar').on('hidden.bs.modal', function () {
+    limpiarCampos();
+});
+
+// Función para limpiar los campos del formulario
+function limpiarCampos() {
+    document.getElementById("Nombre").value = "";
+    document.getElementById("Apellido").value = "";
+    document.getElementById("Email").value = "";
+    document.getElementById("DUI").value = "";
+    document.getElementById("Contraseña").value = "";
+    document.getElementById("Rol").value = "";
+}
+
+// Maneja el clic en el botón "Eliminar"
+$('.eliminar-btn').click(function() {
+        var userId = $(this).data('id');
+        // Muestra un mensaje de confirmación antes de eliminar el usuario (caso en js)
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "¡No podrás revertir esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminarlo',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $('#delete_id').val(userId);
+                $('#delete-form').submit();
+            }
+        });
+    });
+
+$('.dropdown-toggle').click(function() {
+    $(this).next('.dropdown-menu').toggleClass('show');
+});
+
+$(document).click(function (e) {
+    var container = $(".dropdown");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.find('.dropdown-menu').removeClass('show');
+    }
+});
+</script>
 </html>
